63725114f9a30c53d7d5b32190519116bb74f2ed
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-11-03 13:25:33 +0100

    Remove OAuth integration
    Won’t be needed in this tool.

--- app.py
+++ app.py
@@ -2,10 +2,8 @@
 
 import flask
 import mwapi
-import mwoauth
 import os
 import random
-import requests_oauthlib
 import string
 import toolforge
 import yaml
@@ -26,12 +24,6 @@ except FileNotFoundError:
     characters = string.ascii_letters + string.digits
     random_string = ''.join(random.choice(characters) for _ in range(64))
     app.secret_key = random_string
-
-if 'oauth' in app.config:
-    oauth_config = app.config['oauth']
-    consumer_token = mwoauth.ConsumerToken(oauth_config['consumer_key'],
-                                           oauth_config['consumer_secret'])
-    index_php = 'https://www.wikidata.org/w/index.php'
 
 
 @app.template_global()
@@ -73,41 +65,6 @@ def user_link(user_name):
             flask.Markup.escape(user_name) +
             flask.Markup(r'</bdi>') +
             flask.Markup(r'</a>'))
-
-
-@app.template_global()
-def authentication_area():
-    if 'oauth' not in app.config:
-        return flask.Markup()
-
-    if 'oauth_access_token' not in flask.session:
-        return (flask.Markup(r'<a id="login" class="navbar-text" href="') +
-                flask.Markup.escape(flask.url_for('login')) +
-                flask.Markup(r'">Log in</a>'))
-
-    access_token = mwoauth.AccessToken(**flask.session['oauth_access_token'])
-    identity = mwoauth.identify(index_php,
-                                consumer_token,
-                                access_token)
-
-    return (flask.Markup(r'<span class="navbar-text">Logged in as ') +
-            user_link(identity['username']) +
-            flask.Markup(r'</span>'))
-
-
-def authenticated_session():
-    if 'oauth_access_token' not in flask.session:
-        return None
-
-    access_token = mwoauth.AccessToken(
-        **flask.session['oauth_access_token'])
-    auth = requests_oauthlib.OAuth1(client_key=consumer_token.key,
-                                    client_secret=consumer_token.secret,
-                                    resource_owner_key=access_token.key,
-                                    resource_owner_secret=access_token.secret)
-    return mwapi.Session(host='https://www.wikidata.org',
-                         auth=auth,
-                         user_agent=user_agent)
 
 
 @app.route('/taxon/<item_id>')
@@ -159,30 +116,6 @@ def index():
         return flask.redirect(flask.url_for('taxon', item_id=flask.request.form['item_id']))
 
     return flask.render_template('index.html')
-
-
-@app.route('/login')
-def login():
-    redirect, request_token = mwoauth.initiate(index_php,
-                                               consumer_token,
-                                               user_agent=user_agent)
-    flask.session['oauth_request_token'] = dict(zip(request_token._fields,
-                                                    request_token))
-    return flask.redirect(redirect)
-
-
-@app.route('/oauth/callback')
-def oauth_callback():
-    request_token = mwoauth.RequestToken(
-        **flask.session.pop('oauth_request_token'))
-    access_token = mwoauth.complete(index_php,
-                                    consumer_token,
-                                    request_token,
-                                    flask.request.query_string,
-                                    user_agent=user_agent)
-    flask.session['oauth_access_token'] = dict(zip(access_token._fields,
-                                                   access_token))
-    return flask.redirect(flask.url_for('index'))
 
 
 def full_url(endpoint, **kwargs):
