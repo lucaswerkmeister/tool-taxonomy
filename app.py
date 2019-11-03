703c87a7cc04b676da4a64bdadbcb81cdc15b4c3
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2019-11-03 11:08:33 +0100

    Initial commit
    Working taxonomy (kinda) under /taxon/<item_id>, rest of the template
    was not removed yet though.

---
+++ app.py
@@ -0,0 +1,277 @@
+# -*- coding: utf-8 -*-
+
+import flask
+import mwapi
+import mwoauth
+import os
+import random
+import requests_oauthlib
+import string
+import toolforge
+import yaml
+
+
+app = flask.Flask(__name__)
+
+user_agent = toolforge.set_user_agent(
+    'taxonomy',
+    email='mail@lucaswerkmeister.de')
+
+__dir__ = os.path.dirname(__file__)
+try:
+    with open(os.path.join(__dir__, 'config.yaml')) as config_file:
+        app.config.update(yaml.safe_load(config_file))
+except FileNotFoundError:
+    print('config.yaml file not found, assuming local development setup')
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters) for _ in range(64))
+    app.secret_key = random_string
+
+if 'oauth' in app.config:
+    oauth_config = app.config['oauth']
+    consumer_token = mwoauth.ConsumerToken(oauth_config['consumer_key'],
+                                           oauth_config['consumer_secret'])
+    index_php = 'https://www.wikidata.org/w/index.php'
+
+
+@app.template_global()
+def csrf_token():
+    if 'csrf_token' not in flask.session:
+        characters = string.ascii_letters + string.digits
+        random_string = ''.join(random.choice(characters) for _ in range(64))
+        flask.session['csrf_token'] = random_string
+    return flask.session['csrf_token']
+
+
+@app.template_global()
+def form_value(name):
+    if 'repeat_form' in flask.g and name in flask.request.form:
+        return (flask.Markup(r' value="') +
+                flask.Markup.escape(flask.request.form[name]) +
+                flask.Markup(r'" '))
+    else:
+        return flask.Markup()
+
+
+@app.template_global()
+def form_attributes(name):
+    return (flask.Markup(r' id="') +
+            flask.Markup.escape(name) +
+            flask.Markup(r'" name="') +
+            flask.Markup.escape(name) +
+            flask.Markup(r'" ') +
+            form_value(name))
+
+
+@app.template_filter()
+def user_link(user_name):
+    user_href = 'https://www.wikidata.org/wiki/User:'
+    return (flask.Markup(r'<a href="' + user_href) +
+            flask.Markup.escape(user_name.replace(' ', '_')) +
+            flask.Markup(r'">') +
+            flask.Markup(r'<bdi>') +
+            flask.Markup.escape(user_name) +
+            flask.Markup(r'</bdi>') +
+            flask.Markup(r'</a>'))
+
+
+@app.template_global()
+def authentication_area():
+    if 'oauth' not in app.config:
+        return flask.Markup()
+
+    if 'oauth_access_token' not in flask.session:
+        return (flask.Markup(r'<a id="login" class="navbar-text" href="') +
+                flask.Markup.escape(flask.url_for('login')) +
+                flask.Markup(r'">Log in</a>'))
+
+    access_token = mwoauth.AccessToken(**flask.session['oauth_access_token'])
+    identity = mwoauth.identify(index_php,
+                                consumer_token,
+                                access_token)
+
+    return (flask.Markup(r'<span class="navbar-text">Logged in as ') +
+            user_link(identity['username']) +
+            flask.Markup(r'</span>'))
+
+
+def authenticated_session():
+    if 'oauth_access_token' not in flask.session:
+        return None
+
+    access_token = mwoauth.AccessToken(
+        **flask.session['oauth_access_token'])
+    auth = requests_oauthlib.OAuth1(client_key=consumer_token.key,
+                                    client_secret=consumer_token.secret,
+                                    resource_owner_key=access_token.key,
+                                    resource_owner_secret=access_token.secret)
+    return mwapi.Session(host='https://www.wikidata.org',
+                         auth=auth,
+                         user_agent=user_agent)
+
+
+@app.route('/taxon/<item_id>')
+def taxon(item_id):
+    taxon_name, tree = load_taxon_tree(item_id)
+    print(tree)
+    return flask.render_template('taxon.html',
+                                 taxon_name=taxon_name,
+                                 tree=tree)
+
+def load_taxon_tree(item_id):
+    if item_id in flask.g.get('tree_cache', {}):
+        return flask.g.tree_cache[item_id]
+    taxon_name, parent_taxon_item_ids = load_taxon(item_id)
+    d = {}
+    for parent_taxon_item_id in parent_taxon_item_ids:
+        parent_taxon_item_name, tree = load_taxon_tree(parent_taxon_item_id)
+        d[parent_taxon_item_name] = tree
+    flask.g.setdefault('tree_cache', {})[item_id] = taxon_name, d
+    return taxon_name, d
+
+def load_taxon(item_id):
+    session = mwapi.Session('https://www.wikidata.org', user_agent=user_agent)
+    result = session.get(action='wbgetentities',
+                         ids=[item_id],
+                         props=['claims'])
+    entity = result['entities'][item_id]
+    taxon_name_statement = entity['claims']['P225'][0]
+    taxon_name = taxon_name_statement['mainsnak']['datavalue']['value']
+    parent_taxon_statements = entity['claims'].get('P171', [])
+    parent_taxon_item_ids = []
+    for parent_taxon_statement in parent_taxon_statements:
+        parent_taxon_mainsnak = parent_taxon_statement['mainsnak']
+        if parent_taxon_mainsnak['snaktype'] == 'value':
+            parent_taxon_item_ids.append(parent_taxon_mainsnak['datavalue']['value']['id'])
+    return taxon_name, parent_taxon_item_ids
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/greet/<name>')
+def greet(name):
+    return flask.render_template('greet.html',
+                                 name=name)
+
+
+@app.route('/praise', methods=['GET', 'POST'])
+def praise():
+    csrf_error = False
+    if flask.request.method == 'POST':
+        if submitted_request_valid():
+            praise = flask.request.form.get('praise', 'praise missing')
+            flask.session['praise'] = praise
+        else:
+            csrf_error = True
+            flask.g.repeat_form = True
+
+    session = authenticated_session()
+    if session:
+        userinfo = session.get(action='query',
+                               meta='userinfo',
+                               uiprop='options')['query']['userinfo']
+        name = userinfo['name']
+        gender = userinfo['options']['gender']
+        if gender == 'male':
+            default_praise = 'Praise him with great praise!'
+        elif gender == 'female':
+            default_praise = 'Praise her with great praise!'
+        else:
+            default_praise = 'Praise them with great praise!'
+    else:
+        name = None
+        default_praise = 'You rock!'
+
+    praise = flask.session.get('praise', default_praise)
+
+    return flask.render_template('praise.html',
+                                 name=name,
+                                 praise=praise,
+                                 csrf_error=csrf_error)
+
+
+@app.route('/login')
+def login():
+    redirect, request_token = mwoauth.initiate(index_php,
+                                               consumer_token,
+                                               user_agent=user_agent)
+    flask.session['oauth_request_token'] = dict(zip(request_token._fields,
+                                                    request_token))
+    return flask.redirect(redirect)
+
+
+@app.route('/oauth/callback')
+def oauth_callback():
+    request_token = mwoauth.RequestToken(
+        **flask.session.pop('oauth_request_token'))
+    access_token = mwoauth.complete(index_php,
+                                    consumer_token,
+                                    request_token,
+                                    flask.request.query_string,
+                                    user_agent=user_agent)
+    flask.session['oauth_access_token'] = dict(zip(access_token._fields,
+                                                   access_token))
+    return flask.redirect(flask.url_for('index'))
+
+
+def full_url(endpoint, **kwargs):
+    scheme = flask.request.headers.get('X-Forwarded-Proto', 'http')
+    return flask.url_for(endpoint, _external=True, _scheme=scheme, **kwargs)
+
+
+def submitted_request_valid():
+    """Check whether a submitted POST request is valid.
+
+    If this method returns False, the request might have been issued
+    by an attacker as part of a Cross-Site Request Forgery attack;
+    callers MUST NOT process the request in that case.
+    """
+    real_token = flask.session.pop('csrf_token', None)
+    submitted_token = flask.request.form.get('csrf_token', None)
+    if not real_token:
+        # we never expected a POST
+        return False
+    if not submitted_token:
+        # token got lost or attacker did not supply it
+        return False
+    if submitted_token != real_token:
+        # incorrect token (could be outdated or incorrectly forged)
+        return False
+    if not (flask.request.referrer or '').startswith(full_url('index')):
+        # correct token but not coming from the correct page; for
+        # example, JS running on https://tools.wmflabs.org/tool-a is
+        # allowed to access https://tools.wmflabs.org/tool-b and
+        # extract CSRF tokens from it (since both of these pages are
+        # hosted on the https://tools.wmflabs.org domain), so checking
+        # the Referer header is our only protection against attackers
+        # from other Toolforge tools
+        return False
+    return True
+
+
+# If you don’t want to handle CSRF protection in every POST handler,
+# you can instead uncomment the @app.before_request decorator
+# on the following function,
+# which will raise a very generic error for any invalid POST.
+# Otherwise, you can remove the whole function.
+# @app.before_request
+def require_valid_submitted_request():
+    if flask.request.method == 'POST' and not submitted_request_valid():
+        return 'CSRF error', 400  # stop request handling
+    return None  # continue request handling
+
+
+@app.after_request
+def deny_frame(response):
+    """Disallow embedding the tool’s pages in other websites.
+
+    If other websites can embed this tool’s pages, e. g. in <iframe>s,
+    other tools hosted on tools.wmflabs.org can send arbitrary web
+    requests from this tool’s context, bypassing the referrer-based
+    CSRF protection.
+    """
+    response.headers['X-Frame-Options'] = 'deny'
+    return response
